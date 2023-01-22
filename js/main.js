f1e2ea55d95bf1a6ceccc61c27f59ae35caa4f65
casio25 <mishakolomiets355@gmail.com>
2023-01-22 23:13:36 +0200

    deleted randomURL()

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 const countOfOffers = 25;
-const countOfComments = 5
+const countOfComments = 5;
+
+const descriptions = ["Це літо було чудовим", "Фотка з минулого року",
+    "Дуже сумую за цими емоціями", "Сподіваюсь, що я там не був лише один раз"];
+
+const names = ["Артем", "Андрій", "Антон", "Богдан", "Микола", "Максим", "Михайло", "Олег", "Олексій",
+    "Іван", "Дмитро", "Василь", "Юрій", "Петро", "Володимир"]
+
+const comments = ["Все відмінно!", "Загалом все непогано. Але не всі.",
+    "Коли ви робите фотографію, добре б прибирати палець із кадру. Зрештою, це просто непрофесійно.",
+    "Моя бабуся випадково чхнула з фотоапаратом у руках і у неї вийшла фотографія краща.",
+    "Я послизнувся на банановій шкірці і впустив фотоапарат на кота і у мене вийшла фотографія краще.",
+    "Обличчя людей на фотці перекошені, ніби їх побивають. Як можна було зловити такий невдалий момент?"];
 
 const data = new Array(countOfOffers).fill(null).map((e,index)=> getOffer(index))
 const comment = new Array(countOfComments).fill(null).map((e, index) => getComment(index))
 
 function getRandomNumber(min, max) {
-    let step1 = max - min + 1;
-    let step2 = Math.random() * step1;
-    let result = Math.floor(step2) + min;
+    const step1 = max - min + 1;
+    const step2 = Math.random() * step1;
+    const result = Math.floor(step2) + min;
 
     return result
 }
@@ -22,22 +34,15 @@ function getRandomArray(arrayLenght, arrayMax) {
 
 }
 
-const descriptions = ["Це літо було чудовим", "Фотка з минулого року", 
-"Дуже сумую за цими емоціями", "Сподіваюсь, що я там не був лише один раз"];
 
 function getRandomDescription() {
-    const randomArrayNumber = getRandomNumber(0, descriptions.length)
+    const randomArrayNumber = getRandomNumber(0, descriptions.length -1)
     const randomDescription = descriptions[randomArrayNumber];
     return randomDescription
 }
 
 const urlArray = getRandomArray(25, 25);
 
-function getRandomURL() {
-    const randomArrayURL = getRandomNumber(0, urlArray.length)
-    const randomURL = descriptions[randomArrayURL];
-    return randomURL
-}
 
 function getOffer(index){
     for (let i = 0; i < urlArray.length; i++) {
@@ -50,26 +55,15 @@ function getOffer(index){
 }
 }
 
-const comments = ["Все відмінно!", "Загалом все непогано. Але не всі.",
-    "Коли ви робите фотографію, добре б прибирати палець із кадру. Зрештою, це просто непрофесійно.",
-    "Моя бабуся випадково чхнула з фотоапаратом у руках і у неї вийшла фотографія краща.",
-    "Я послизнувся на банановій шкірці і впустив фотоапарат на кота і у мене вийшла фотографія краще.",
-    "Обличчя людей на фотці перекошені, ніби їх побивають. Як можна було зловити такий невдалий момент?"];
-
-
-
-const names = ["Артем", "Андрій", "Антон", "Богдан", "Микола", "Максим", "Михайло", "Олег", "Олексій",
-"Іван", "Дмитро", "Василь", "Юрій", "Петро", "Володимир"]
-
 function getRandomComment() {
-    const randomArrayNumber = getRandomNumber(0, comments.length)
+    const randomArrayNumber = getRandomNumber(0, comments.length -1)
     const RandomComment = comments[randomArrayNumber];
     return RandomComment
 }
 
 function getRandomName() {
-    const randomArrayNumber = getRandomNumber(0, names.length)
-    const RandomComment = names[randomArrayNumber];
+    const randomArrayNumber = getRandomNumber(0, names.length -1)
+    const RandomName = names[randomArrayNumber];
     return RandomName
 }
 
