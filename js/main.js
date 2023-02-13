868a1c1bd112b9887bfeed99cd967ae73bff39d8
casio25 <mishakolomiets355@gmail.com>
2023-02-13 22:26:11 +0200

    added backend

--- js/main.js
+++ js/main.js
@@ -1,54 +1,13 @@
 import {showBigPicture} from "./bigPhoto.js";
 import { ImageUpload } from "./validation.js";
 
-const countOfOffers = 25;
-const countOfComments = 25;
-const countOfAvatars = 6;
-const descriptions = ["Це літо було чудовим", "Фотка з минулого року",
-    "Дуже сумую за цими емоціями", "Сподіваюсь, що я там не був лише один раз"];
-
-const names = ["Артем", "Андрій", "Антон", "Богдан", "Микола", "Максим", "Михайло", "Олег", "Олексій",
-    "Іван", "Дмитро", "Василь", "Юрій", "Петро", "Володимир"]
-
-const comments = ["Все відмінно!", "Загалом все непогано. Але не всі.",
-    "Коли ви робите фотографію, добре б прибирати палець із кадру. Зрештою, це просто непрофесійно.",
-    "Моя бабуся випадково чхнула з фотоапаратом у руках і у неї вийшла фотографія краща.",
-    "Я послизнувся на банановій шкірці і впустив фотоапарат на кота і у мене вийшла фотографія краще.",
-    "Обличчя людей на фотці перекошені, ніби їх побивають. Як можна було зловити такий невдалий момент?"];
-
-const data = new Array(countOfOffers).fill(null).map((e, index) => {
-    return getOffer(index);
-});
-const commentArray = new Array(countOfComments).fill(null).map((e, index) => getComment(index))
-
-
-
-function getRandomNumber(min, max) {
-    const step1 = max - min + 1;
-    const step2 = Math.random() * step1;
-    const result = Math.floor(step2) + min;
-
-    return result
-}
-
-function getRandomArray(arrayLenght, arrayMax) {
-    const arr = []
-    while (arr.length < arrayLenght) {
-        const r = Math.floor(Math.random() * arrayMax) + 1;
-        if (arr.indexOf(r) === -1) arr.push(r);
-    }
-    return arr;
-
-}
-
-
-
-function getRandomDescription() {
-    const randomArrayNumber = getRandomNumber(0, descriptions.length -1)
-    const randomDescription = descriptions[randomArrayNumber];
-    return randomDescription
-}
-
+const data = await fetch('http://localhost:3000/data')
+    .then(function (resp) {
+        return resp.json()
+    })
+    .catch((error) => {
+        return `${error}`;
+    });
 
 
 function getOffer(index){
@@ -67,7 +26,7 @@ function getComment(countOfComments,) {
     const ArrayOfComments=[];
     for (let i = 0; i < countOfComments; i++) {
         ArrayOfComments.push({
-            id: "c"+getRandomNumber(1, 999),
+            id: getRandomNumber(1, 999),
             avatar: `img/avatar-${getRandomNumber(1, countOfAvatars)}.svg`,
             comment: comments[getRandomNumber(1, comments.length-1)],
             name: names[getRandomNumber(1, names.length-1)]
@@ -76,6 +35,7 @@ function getComment(countOfComments,) {
         }
     return ArrayOfComments;
     }
-export{data, commentArray};
+export{data};
 showBigPicture(data);
 ImageUpload();
+console.log(data);
